a34c731300931520434de7b23800d2e1fdc5b20f
tarques <tarcilatorresmt@gmail.com>
2024-09-10 17:19:42 -0300

    teste de java console

---
+++ aula.js
@@ -0,0 +1,15 @@
+let a = 10
+let b = 5
+
+let soma = a + b           
+let subtracao = a - b      
+let multiplicacao = a * b  
+let divisao = a / b       
+let restoDivisao = a % b   
+
+
+document.write("soma: " + soma)                
+console.log("subtração: " + subtracao)      
+console.log("multiplicação: " + multiplicacao) 
+console.log("divisão: " + divisao)         
+console.log("resto da divisão: " + restoDivisao)
